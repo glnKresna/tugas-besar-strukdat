bdd5c796b716e2ab79d8af0479eebb5edf88ec61
glnKresna <prabandikakresna@gmail.com>
2025-12-29 18:58:06 +0700

    do-while

--- ARSIFILM/main.cpp
+++ ARSIFILM/main.cpp
@@ -15,64 +15,72 @@ int main() {
     int tahun, durasi;
     float rating;
 
-    cout << "\n===== SISTEM ARSIP FILM =====\n";
-    cout << "1. Tambah Data Film\n";
-    cout << "2. Cari Film (Judul)\n";
-    cout << "3. Hapus Film\n";
-    cout << "4. Tampilkan Semua Film (A - Z)\n";
-    cout << "0. Keluar\n";
-    cout << "Pilih menu: ";
-    cin >> pilihan;
-    cin.ignore();
+    do {
+        cout << "\n===== SISTEM ARSIP FILM =====\n";
+        cout << "1. Tambah Data Film\n";
+        cout << "2. Cari Film (Judul)\n";
+        cout << "3. Hapus Film\n";
+        cout << "4. Tampilkan Semua Film (A - Z)\n";
+        cout << "0. Keluar\n";
+        cout << "Pilih menu: ";
+        cin >> pilihan;
+        cin.ignore();
 
-    switch (pilihan) {
-    case 1:
-        cout << "Judul Film   : ";
-        getline(cin, judul);
-        cout << "Genre        : ";
-        getline(cin, genre);
-        cout << "Tahun Tayang : ";
-        cin >> tahun;
-        cout << "Durasi (mnt) : ";
-        cin >> durasi;
-        cout << "Rating       : ";
-        cin >> rating;
+        switch (pilihan) {
+        case 1:
+            cout << "Judul Film   : ";
+            getline(cin, judul);
+            cout << "Genre        : ";
+            getline(cin, genre);
+            cout << "Tahun Tayang : ";
+            cin >> tahun;
+            cout << "Durasi (mnt) : ";
+            cin >> durasi;
+            cout << "Rating       : ";
+            cin >> rating;
+            cin.ignore();
 
-        insertFilm(filmBST, createNode(judul, genre, tahun, durasi, rating));
-        cout << "Film berhasil ditambahkan!\n";
-        break;
+            insertFilm(filmBST, createNode(judul, genre, tahun, durasi, rating));
+            cout << "Film berhasil ditambahkan!\n";
+            break;
 
-    case 2:
-        cout << "Masukkan judul film: ";
-        getline(cin, judul);
-        {
-            filmData* hasil = searchByJudul(filmBST.root, judul);
-            if (hasil != NULL) {
-                cout << "Film ditemukan!\n";
-                cout << "Judul  : " << hasil->judul << endl;
-                cout << "Rating : " << hasil->rating << endl;
-                cout << "Genre  : " << hasil->genre << endl;
-                cout << "Tahun  : " << hasil->tahunTayang << endl;
-                cout << "Rating : " << hasil->rating << endl;
-            } else {
-                cout << "Film tidak ditemukan.\n";
+        case 2:
+            cout << "Masukkan judul film: ";
+            getline(cin, judul);
+            {
+                filmData* hasil = searchByJudul(filmBST.root, judul);
+                if (hasil != NULL) {
+                    cout << "\nFilm ditemukan!\n";
+                    cout << "Judul  : " << hasil->judul << endl;
+                    cout << "Genre  : " << hasil->genre << endl;
+                    cout << "Tahun  : " << hasil->tahunTayang << endl;
+                    cout << "Rating : " << hasil->rating << endl;
+                } else {
+                    cout << "Film tidak ditemukan.\n";
+                }
             }
+            break;
+
+        case 3:
+            cout << "Judul film yang dihapus: ";
+            getline(cin, judul);
+            filmBST.root = deleteByJudul(filmBST.root, judul);
+            cout << "Data film diproses.\n";
+            break;
+
+        case 4:
+            inorder(filmBST.root);
+            break;
+
+        case 0:
+            cout << "Keluar dari program.\n";
+            break;
+
+        default:
+            cout << "Pilihan tidak valid.\n";
         }
-        break;
 
-    case 3:
-        cout << "Judul film yang dihapus: ";
-        getline(cin, judul);
-        filmBST.root = deleteByJudul(filmBST.root, judul);
-        cout << "Data film diproses.\n";
-        break;
+    } while (pilihan != 0);
 
-    case 4:
-        inorder(filmBST.root);
-        break;
-
-    case 0:
-        cout << "Keluar dari program.\n";
-        break;
-    }
-} 
+    return 0;
+}
